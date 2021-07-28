0186833f542bd730f1fe54f18a6a7aecf2cb48e5
Daniel-VM <da.valle@isciii.es>
2021-07-28 09:01:38 +0200

    Bash script to dowload sRNAnalyzer DBs

---
+++ bin/scrapping_dbs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## This databases takes long to be downloaded. Therefore we will set a new screen environment to download all of them by using a siple sh script
+
+OUTDIR=/home/da.valle/work/amandafr/osmRNAseq/sranalyzer/DBs/bowtie/indexes
+
+#wget -P $OUTDIR http://srnanalyzer.systemsbiology.net/downloads/sRNA_DBs.tar.gz
+#wget -P $OUTDIR http://srnanalyzer.systemsbiology.net/downloads/MainDBs.tar.gz
+#wget -P $OUTDIR http://srnanalyzer.systemsbiology.net/downloads/NCBI_NonHuman.tar.gz
+#wget -P $OUTDIR http://srnanalyzer.systemsbiology.net/downloads/E_coli_evaluation_samples.tar.gz
+
+# Move to DB dir
+cd $OUTDIR
+
+# Extract tar.gz dfiles
+for i in *.tar.gz
+do
+	tar -xvf $i
+done
+
+# Return to sh execution directory
+cd -
